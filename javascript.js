5a233b1ba3d1704e4d8e4d3deac900a1394ef925
AAKASH HANDA <AAKASH HANDA>
2017-05-17 21:16:16 +0530

    a

---
+++ javascript.js
@@ -0,0 +1,121 @@
+string
+number
+bool
+unf
+null
+
+var a = "brindha"
+var a = 10
+var a = true
+
+1+1
+2
+"birndha"+" javascript"
+"birndha javascript"
+"birndha" + 10
+"birndha10"
+1-1
+0
+8/2
+4
+4*4
+16
+
+
+1 == "1"
+true
+1 === "1"
+false
+console.log("dfdfgf")
+VM242:1 dfdfgf
+undefined
+alert("h1")
+
+
+
+
+
+var num = 10;
+if(num%2 == 0){
+	console.log(num+ " is even")
+}
+else{
+	console.log(num+ " is odd")
+}
+
+function isEven(a){
+	if(a%2 == 0){
+	console.log(a+ " is even")
+	}
+	else{
+		console.log(a+ " is odd")
+	}
+}
+isEven(99)
+
+
+for(i=0; i<=10;i++){
+	if(i%2 == 0){
+	console.log(i+ " is even")
+	}
+	else{
+		console.log(i+ " is odd")
+	}
+}
+
+
+
+var a = [1,2,3,4,5];
+var a = ["df","fd","fh"];
+
+var b = [1,2,"3a",true,"fgbgf",4,5]
+
+var c = [1,2,3,4,5]
+for(i=0; i<= c.length;i++){
+	console.log(c[i])
+}
+
+var d = [1,2,3[4,5,[6,7],8],9]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
